3f08b2347600c26fba15bfc617ca7559eca640b6
nico-brehm <nico-brehm@users.noreply.github.com>
2017-07-03 19:40:32 +0200

    adapted to the new task message structure

--- Transportservice/app.js
+++ Transportservice/app.js
@@ -1,16 +1,18 @@
+var conf = require('./config.json');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/TransportDB');
+var db = monk(conf.db);
+console.log("connected to mongo_db at: "+conf.db);
 
-const mqtt = require('mqtt')  
-const client = mqtt.connect('mqtt:localhost')
-
+const mqtt = require('mqtt');  
+const client = mqtt.connect(conf.mqttbroker);
 
 var routes = require('./routes/index');
 var tsdata = require('./routes/tsdata');
@@ -39,17 +41,6 @@ app.use(function(req,res,next){
 app.use('/', routes);
 app.use('/tsdata', tsdata);
 
-//Subscribe to Broker
-client.on('connect', () => {  
-  client.subscribe('workflow/claim/#')
-})
-//JSON in db speichern
-client.on('message', (topic, message) => {  
-    console.log(message.toString());
-	var newMessage = JSON.parse(message);
-	db.collection('tsdatalist').insert(newMessage);
-});
-
 
 // 404
 app.use(function(req, res, next) {
@@ -66,6 +57,23 @@ app.use(function(err, req, res, next) {
         message: err.message,
         error: {}
     });
+});
+
+//Subscribe to Broker
+client.on('connect', () => {  
+  console.log("connected to mqtt broker at: "+conf.mqttbroker);
+  client.subscribe('workflow/claim/#')
+})
+//write task objects to db
+client.on('message', (topic, message) => {  
+    console.log("reveived a new message: "+message.toString());
+	//expected message srtucture: (claimedtasks: [{"id":"657373", "workingstation":"65747", "productId":"27"}],{...},{...})
+	var newMessage = JSON.parse(message);
+	for(thisTask of newMessage.claimedtasks)
+	{
+		console.log("found new claimed task.... "+JSON.stringify(thisTask));
+		db.collection('tsdatalist').insert(thisTask);
+	}
 });
 
 
--- Transportservice/bin/www.js
+++ Transportservice/bin/www.js
@@ -1,8 +1,9 @@
-#!/usr/bin/env node
+var conf = require('../config.json');
 var debug = require('debug')('Transportservice');
 var app = require('../app');
 
-app.set('port', process.env.PORT || 3002);
+
+app.set('port', process.env.PORT || conf.port);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
